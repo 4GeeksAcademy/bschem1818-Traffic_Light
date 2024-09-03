04e31f1c4d553a49e4c691d671fcc77f598c994e
Bschem1818 <barbaraschem@gmail.com>
2024-09-03 06:30:40 +0000

    updated

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,64 @@
-import React from "react";
+import React, { useState } from 'react';
+import ReactDOM from "react-dom/client";
+
+const Home = ()=> {
+    const [ color, setColor] = useState("");
+    const [ YboxShadow, setYboxShadow] = useState('0px 0px 9px 4px #000000')
+    const [ RboxShadow, setRboxShadow] = useState('0px 0px 9px 4px #000000')
+    const [ GboxShadow, setGboxShadow] = useState('0px 0px 9px 4px #000000')
+
+
+function changeColor(c) {
+    setColor(c)
+    if (color == 'yellow')
+        {
+            setYboxShadow('0px 0px 9px 4px #747DE8')
+            setGboxShadow('0px 0px 9px 4px #000000')
+            setRboxShadow('0px 0px 9px 4px #000000')
+
+        }
+         else if (color == 'red')
+            {
+                setYboxShadow('0px 0px 9px 4px #000000')
+                setGboxShadow('0px 0px 9px 4px #000000')
+                setRboxShadow('0px 0px 9px 4px #747DE8')
+    
+            }
+
+         else if (color == 'green')
+            {
+                setYboxShadow('0px 0px 9px 4px #000000')
+                setGboxShadow('0px 0px 9px 4px #747DE8')
+                setRboxShadow('0px 0px 9px 4px #000000')
+    
+            }
+}
+
+
+
+    return (
+<div>  
+    <div className="container">
+        <div className="holder"></div>
+    </div>
+    <div className="container">
+        <div className="traffic-light">
+            <div style={{ boxShadow: RboxShadow}} onClick={() => changeColor('red')} className="light red"></div>
+            <div style={{ boxShadow: YboxShadow}}  onClick={() => changeColor('yellow')} className="light yellow"></div>
+            <div style={{ boxShadow: GboxShadow}} onClick={() => changeColor('green')} className="light green"></div>	
+        </div>
+    </div>  
+</div>
+
+    );
+};
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+
 
 export default Home;
